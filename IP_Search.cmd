8a55592ea3e2082a6bc146fddd78e277e19d84be
ScriptTiger <thescripttiger@gmail.com>
2017-09-08 00:17:07 +0800

    Commented script flow

--- IP_Search.cmd
+++ IP_Search.cmd
@@ -7,6 +7,10 @@ rem Or visit the following URL for the latest information on this ScriptTiger sc
 rem https://github.com/ScriptTiger/IP
 rem =====
 
+rem =====
+rem Environmental setup
+rem =====
+
 setlocal ENABLEDELAYEDEXPANSION
 
 set DATA=%~dp0GeoLite2
@@ -16,7 +20,12 @@ set ASN4=%DATA%\GeoLite2-ASN-Blocks-IPv4.csv
 set ASN6=%DATA%\GeoLite2-ASN-Blocks-IPv6.csv
 set NETCALC=%~dp0Network_Calc.cmd
 
+rem Set default mode as interactive
 set INTERACTIVE=1
+
+rem =====
+rem Set language preference
+rem =====
 
 if "%~1"=="" (
 	echo 1.] Deutsche
@@ -39,21 +48,41 @@ if "%~1"=="" (
 ) else set LANG=%~1
 set LANG=%DATA%\GeoLite2-City-Locations-%LANG%.csv
 
+rem =====
+rem Set IP to search for
+rem =====
+
 if "%~2"=="" (set /p IP=IP: ) else (
 	set IP=%~2
 	set INTERACTIVE=0
 )
+
+rem =====
+rem Build search string of all possible networks
+rem =====
 
 set FINDSTR=
 for /l %%0 in (0,1,32) do (
 	for /f %%a in ('%NETCALC% /id %IP%/%%0') do set FINDSTR=!FINDSTR!%%a, 
 )
 
+rem =====
+rem Search city data for any of the possible networks
+rem =====
+
 set CITY=
 call :Find "%FINDSTR%" "%CITY4%" CITY
 
+rem =====
+rem Search ASN data for any of the possible networks
+rem =====
+
 set ASN=
 call :Find "%FINDSTR%" "%ASN4%" ASN
+
+rem =====
+rem Display matching city data
+rem =====
 
 if not "!CITY!"=="" (
 	for /f "tokens=2 delims=," %%a in ('echo !CITY!') do set CITY=%%a
@@ -61,16 +90,27 @@ if not "!CITY!"=="" (
 	echo City:	!CITY!
 ) else echo No City Data found for this Public IP
 
+rem =====
+rem Display matching ASN data
+rem =====
+
 if not "!ASN!"=="" (
-	for /f "tokens=2* delims=," %%a in ('echo !ASN!') do set ISP=%%~b
-	for /f "tokens=2 delims=," %%a in ('echo !ASN!') do set ASN=%%a
+	for /f "tokens=2* delims=," %%a in ('echo !ASN!') do (
+		set ASN=%%a
+		set ISP=%%~b
+	)
 	echo ASN:	!ASN!
 	echo ISP:	!ISP!
 ) else echo No ASN Data found for this Public IP
 
+rem Pause before exiting if in interactive mode
 if %INTERACTIVE%==1 pause
 
 exit /b
+
+rem =====
+rem Function for searching MaxMind data for entries matching any of the possible networks
+rem =====
 
 :Find
 for /f "tokens=*" %%0 in ('findstr /b /l "%~1" "%~2"') do set %~3=%%0
