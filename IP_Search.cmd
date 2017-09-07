9e8e57b3a4e9bdb9cea1e587b56e5e13b904e538
ScriptTiger <thescripttiger@gmail.com>
2017-09-07 14:42:52 +0800

    Multilingual and scripted search support

--- IP_Search.cmd
+++ IP_Search.cmd
@@ -14,40 +14,71 @@ set CITY4=%DATA%\GeoLite2-City-Blocks-IPv4.csv
 set CITY6=%DATA%\GeoLite2-City-Blocks-IPv6.csv
 set ASN4=%DATA%\GeoLite2-ASN-Blocks-IPv4.csv
 set ASN6=%DATA%\GeoLite2-ASN-Blocks-IPv6.csv
-set LANG=en
-set LANG=%DATA%\GeoLite2-City-Locations-%LANG%.csv
 set NETCALC=%~dp0Network_Calc.cmd
 
-set /p IP=IP: 
+set INTERACTIVE=1
 
-:GeoIP
-for /l %%0 in (32,-1,0) do (
-	for /f %%a in ('%NETCALC% /id %IP%/%%0') do set NETWORK=%%a
-	call :Find !NETWORK! "%CITY4%" GeoIP ASN
+if "%~1"=="" (
+echo 1.] Deutsche
+echo 2.] English
+echo 3.] Español
+echo 4.] Français
+echo 5.] Nihongo
+echo 6.] Portugues ^(Brasil^)
+echo 7.] Russkiy
+echo 8.] Zhongwén ^(Jianhuàzì^)
+choice /c 12345678 /n
+if !errorlevel!==1 set LANG=de
+if !errorlevel!==2 set LANG=en
+if !errorlevel!==3 set LANG=es
+if !errorlevel!==4 set LANG=fr
+if !errorlevel!==5 set LANG=ja
+if !errorlevel!==6 set LANG=pt-BR
+if !errorlevel!==7 set LANG=ru
+if !errorlevel!==8 set LANG=zh-CN
+) else set LANG=%~1
+set LANG=%DATA%\GeoLite2-City-Locations-%LANG%.csv
+
+if "%~2"=="" (set /p IP=IP: ) else (
+set IP=%~2
+set INTERACTIVE=0
 )
 
-echo No GeoIP Data found for this Public IP
+:City
+set CITY=
+for /l %%0 in (32,-1,0) do (
+	for /f %%a in ('%NETCALC% /id %IP%/%%0') do set NETWORK=%%a
+	call :Find !NETWORK! "%CITY4%" CITY
+	if not "!CITY!"=="" goto ASN
+)
 
 :ASN
+set ASN=
 for /l %%0 in (32,-1,0) do (
 	for /f %%a in ('%NETCALC% /id %IP%/%%0') do set NETWORK=%%a
-	call :Find !NETWORK! "%ASN4%" ASN Exit
+	call :Find !NETWORK! "%ASN4%" ASN
+	if not "!ASN!"=="" goto Exit
 )
-echo No ASN Data found for this Public IP
-goto Exit
-
-:Find
-(for /f "tokens=*" %%a in ('findstr "^%~1" "%~2"') do set %~3=%%a) && goto %~4
-exit /b
 
 :Exit
-for /f "tokens=2 delims=," %%a in ('echo !GeoIP!') do set CITY=%%a
-for /f "tokens=3 delims=," %%a in ('echo !GeoIP!') do set COUNTRY=%%a
-for /f "tokens=2* delims=," %%a in ('echo !ASN!') do set ASN=%%~b
-for /f "tokens=*" %%a in ('findstr "^%CITY%" "%LANG%"') do set CITY=%%a
-for /f "tokens=*" %%a in ('findstr "^%COUNTRY%" "%LANG%"') do set COUNTRY=%%a
-echo City Data:	!CITY!
-echo Country Data:	!COUNTRY!
-echo ASN Data:	!ASN!
-pause
-exit
+
+if not "!CITY!"=="" (
+for /f "tokens=2 delims=," %%a in ('echo !CITY!') do set CITY=%%a
+for /f "tokens=2* delims=," %%a in ('findstr /b "!CITY!" "%LANG%"') do set CITY=%%b
+echo City:	!CITY!
+) else echo No City Data found for this Public IP
+
+if not "!ASN!"=="" (
+for /f "tokens=2* delims=," %%a in ('echo !ASN!') do set ISP=%%~b
+for /f "tokens=2 delims=," %%a in ('echo !ASN!') do set ASN=%%a
+echo ASN:	!ASN!
+echo ISP:	!ISP!
+) else echo No ASN Data found for this Public IP
+
+if %INTERACTIVE%==1 pause
+
+exit /b
+
+:Find
+for /f "tokens=*" %%a in ('findstr /b "%~1" "%~2"') do set %~3=%%a
+exit /b
